1a338bc36174cdd9699400a3316debf7f7db202a
Panjigumelar54 <134267886+Panjigumelar54@users.noreply.github.com>
2023-06-12 14:36:49 +0800

    Melengkapi method Matematika

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -13,5 +13,21 @@ public class Matematika {
     //membuat method setPenjumlahan
     double setPenjumlahan(){
         return bil1 + bil2;
+    }  
+    
+    
+    //membuat method setPengurangan
+    double setPengurangan(){
+        return bil1 - bil2;
+    }
+      
+    //membuat method setPerkalian
+    double setPerkalian(){
+        return bil1 * bil2;
+    }
+      
+    //membuat method setPembagian
+    double setPembagian(){
+        return bil1 / bil2;
     }
 }
